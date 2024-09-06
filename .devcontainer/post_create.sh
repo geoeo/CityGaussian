aae797dd77f3e1594bbd4b8584444e238607e6b1
marc haubenstock <marc.dhaubenstock@gmail.com>
2024-09-06 11:45:14 +0000

    added todo

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -5,5 +5,6 @@ mkdir output
 conda run -n citygs --no-capture-output pip install --upgrade-strategy only-if-needed -r requirements.txt
 cd LargeLightGaussian
 conda run -n citygs --no-capture-output pip install submodules/compress-diff-gaussian-rasterization
+#TODO fix this so that it works when already created
 ln -sf /workspaces/CityGaussian/data /workspaces/CityGaussian/LargeLightGaussian/data
 ln -sf /workspaces/CityGaussian/output /workspaces/CityGaussian/LargeLightGaussian/output
